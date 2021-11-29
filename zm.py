763122874a5f22844d06369c9f36f3221fd23bc4
xocky <nikitossaveliew@gmail.com>
2021-11-29 23:35:42 +0300

    gf

--- zm.py
+++ zm.py
@@ -1,4 +1,25 @@
-print(' '*3 + '*' + ' '*3 )
-print(' '*2 + '***' + ' '*2)
-print(' ' + '*****' + ' ')
-print('*******')
+from tkinter import *
+from random import randint
+root = Tk()
+
+canv = Canvas(root, width=900, height=900)
+
+colors=['green', 'blue', 'black']
+
+def func (event):
+    num = randint (1, len(colors))
+    cl = colors[num - 1]
+    wd = randint(20, 60)
+    ht = randint(20, 60)
+    x = event.x
+    y = event.y
+    circle = canv.create_oval(x, y, x + wd, y + ht, fill=cl, outline='black')
+    rect = canv.create_rectangle(x, y, x + wd, y + ht, fill=cl, outline='black')
+    print(cl)
+
+canv.bind('<Button-1>', func)
+canv.pack()
+root.mainloop()
+
+
+
